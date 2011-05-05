402485f912ea0fe5fdf86758db4d98fde671df43
Heimir Þór Kjartansson <hemmi.hk@gmail.com>
2011-05-05 12:03:37 +0000

    Beaming Ball java online applet added.

---
+++ BallApplet.java
@@ -0,0 +1,62 @@
+import java.applet.*;
+import java.awt.*;
+
+public class BallApplet extends Applet implements Runnable {
+    int x_pos=30;
+    int y_pos=100;
+    int x_speed=1;
+    int radius=20;
+    int appletsize_x=300;
+    int appletsize_y=300;
+
+    private Image dbImage;
+    private Graphics dbg;
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+    public void start() {
+        Thread th = new Thread(this);
+        th.start();
+    }
+    public void stop() {}
+    public void destroy() {}
+    public void run() {
+        Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+        while(true) {
+            //if(x_pos>appletsize_x-radius) {
+            //    x_speed=-1;
+            //}
+            if(x_pos>(appletsize_x+radius)) {
+                x_pos=-20;
+            }
+            //else if(x_pos<radius) {
+            //   x_speed+=1;
+            //}
+            x_pos+=x_speed;
+            repaint();
+            try {
+                Thread.sleep(20);
+            }
+            catch(InterruptedException ex) {
+                // do nothing
+            }
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+        }
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(x_pos-radius,y_pos-radius,2*radius,2*radius);
+    }
+    public void update(Graphics g) {
+        if(dbImage==null) {
+            dbImage=createImage(this.getSize().width,this.getSize().height);
+            dbg=dbImage.getGraphics();
+        }
+        dbg.setColor(getBackground());
+        dbg.fillRect(0,0,this.getSize().width,this.getSize().height);
+        dbg.setColor(getForeground());
+        paint(dbg);
+        g.drawImage(dbImage,0,0,this);
+    }
+}
